85a3a2bf55146b12dfde871a840af4245ecaa6c8
Lee <leesheppard2404@gmail.com>
2023-07-17 12:56:27 +0200

    feat(db): connected to mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
-  return <h1>Watchlist</h1>;
+  return (
+    <>
+      <ul>
+        <li>trade</li>
+        <li>learn</li>
+      </ul>
+      <input
+        type="text"
+        name="searchbar"
+        id="searchbar"
+        placeholder="Search for a stock"
+      />
+      <h2>watchlist</h2>
+      <ul>
+        <li>tsla</li>
+        <li>tsla</li>
+        <li>tsla</li>
+        <li>tsla</li>
+      </ul>
+      <p>
+        <Link href={'login'}>Login</Link> or
+        <Link href={'signup'}>signup</Link> to see your portfolio
+      </p>
+    </>
+  );
 }
