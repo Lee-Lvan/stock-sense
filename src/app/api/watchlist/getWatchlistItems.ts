a80457ba61fc4229af3e5e3d853b4b7fd52b10b4
Lee <leesheppard2404@gmail.com>
2023-07-17 21:07:46 +0200

    refactor(): refactored code

--- src/app/api/watchlist/getWatchlistItems.ts
+++ src/app/api/watchlist/getWatchlistItems.ts
@@ -1,10 +1,7 @@
-import { connectToMongo } from '../../utils/connectToMongo';
 import watchlistItems from '../../models/watchlistItemsModel';
 
 const getWatchlist = async () => {
-  connectToMongo();
-  const watchlist = await watchlistItems.find({});
-  return watchlist;
+  return await watchlistItems.find({});
 };
 
 export default getWatchlist;
