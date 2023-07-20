0f4cddf86ecdb28511872c4e6f8f6fe9c3ef75ac
gnilvan <gnilvan@users.noreply.github.com>
2023-07-20 20:35:57 +0200

    continue with google and github implemented

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    try {
+      const data = await signIn('credentials', {
+        redirect: false,
+        email: emailRef.current?.value as string,
+        password: passwordRef.current?.value as string,
+      });
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    };
+  };
+
+  return (
+        <>
+        <h1>StockSense</h1>
+        <h3>Sign up</h3>
+        <p>Already have an account?</p>
+        <Link href={'/login'}>Log in</Link>
+        <button onClick={() => signIn('github')}>Github</button>
+        <button onClick={() => signIn('google')}>Google</button>
+        <hr />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={emailRef}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={passwordRef}/>
+          <button type='submit'>Sign up</button>
+        </form>
+        </>
+  );
 };
 
 export default Signup;
