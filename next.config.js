be68c8674fd226da8391395968e29245d21a5c15
gnilvan <gnilvan@users.noreply.github.com>
2023-07-21 09:29:45 +0200

    typo fixed with github secrect

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     // NEXTAUTH_SECRET: ,
 
     GITHUB_ID: 'fa2c2086e7721fd870a1',
-    GITHUB_SECRE: 'fc2de468527bb7abe26f7a08aed7dfacfd1c5e78',
+    GITHUB_SECRET: 'fc2de468527bb7abe26f7a08aed7dfacfd1c5e78',
 
     GOOGLE_ID:'744014055280-dn269s9vs10u5ufqv9j5um5vfpfcu6sp.apps.googleusercontent.com',
     GOOGLE_SECRET:'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',
