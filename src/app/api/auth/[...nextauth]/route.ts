217cfdd4ef21cb8d90e5ddee008fea4bada379e0
Lee <leesheppard2404@gmail.com>
2023-07-22 19:53:59 +0200

    removed comments

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,16 +15,4 @@ const handler = NextAuth({
   ],
 })
 
-
-
-
-  // A database is optional, but required to persist accounts in a database
-  // database: process.env.DATABASE_URL,
-  // database: {
-  //   type: 'mongodb',
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  //   url: process.env.DATABASE_URL,
-  // },
-
 export { handler as GET, handler as POST };
