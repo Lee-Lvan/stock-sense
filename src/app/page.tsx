bd38e5da3ceb11df2b4e08aca478fec92331253e
Lee <leesheppard2404@gmail.com>
2023-07-23 20:08:07 +0200

    added date to app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,25 @@ export default async function Home() {
   const response = await getDefaultWatchlist()
   const defaultData = response.map(item => item.name).join(',') 
 
+  
+const getFormattedDate = () => {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const today = new Date();
+  const dayOfWeek = daysOfWeek[today.getDay()];
+  const month = months[today.getMonth()];
+  const date = today.getDate();
+
+  return `${dayOfWeek} ${month} ${date}`;
+}
+
+const date = getFormattedDate();
+
   return (
     <>
       <Navbar />
+      <p>{date}</p>
       <Searchbar />
       <Dashboard defaultData={defaultData}/>
     </>
