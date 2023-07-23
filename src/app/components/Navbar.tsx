d4f80f2837ed57ee627921c3eb4c0d86b54f8b94
Lee <leesheppard2404@gmail.com>
2023-07-23 20:23:57 +0200

    added signin sign out to navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,19 @@
+'use client'
 import React from 'react'
+import { useSession, signOut } from 'next-auth/react';
+import Link from 'next/link';
 
 const Navbar = () => {
+  const { data: session } = useSession();
+
   return (
     <nav>
       <ul>
         <li>TRADE</li>
         <li>StockSense</li>
         <li>LEARN</li>
+        {!session &&<li><Link href={'/signin'}>Log in/sign up</Link></li>}
+        {session && <li onClick={() => signOut()}>Sign out</li>}
       </ul>
     </nav>
   )
