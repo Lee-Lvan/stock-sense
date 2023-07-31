d7650758a433a56059135451709d5b9ce3af4760
gnilvan <gnilvan@users.noreply.github.com>
2023-07-31 14:44:10 +0200

    linkedin link added in the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
 const Footer = () => {
   return (
     <footer>
-        <p className="mark">Design & Developed by Lee and Lvan</p>
-        <p className="stack">Powered by Next.js, MongoDB, TwelveData, ApexChart</p>
-        <br />
-        <p className="copyright">© 2023 StockSense Inc.</p>
+      <p className="mark">
+        Design & Developed by{' '}
+        <a href="https://www.linkedin.com/in/leesheppard244/" className="linkedin">
+          Lee Sheppard
+        </a>{' '}
+        and{' '}
+        <a href="https://www.linkedin.com/in/lvan-ni/" className="linkedin">
+          Lvan Ni
+        </a>
+      </p>
+      <p className="stack">Powered by Next.js, MongoDB, TwelveData, ApexChart</p>
+      <br />
+      <p className="copyright">© 2023 StockSense Inc.</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
