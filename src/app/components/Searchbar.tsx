9c52f9c141ebe5725b26264ffffc49a34ac5df7d
gnilvan <gnilvan@users.noreply.github.com>
2023-08-13 22:19:00 +0200

    demo image added

---
+++ src/app/components/Searchbar.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React, { useState } from 'react';
+import { IStock } from '../interfaces/IStock';
+import Link from 'next/link';
+
+const Searchbar = () => {
+  const [results, setResults] = useState<IStock[]>([]);
+
+  const handleSetQuery = async (query: string) => {
+    const response = await fetch(`/api/symbols?query=${query}`);
+    const symbols = await response.json();
+    setResults(symbols);
+  };
+  return (
+    <>
+      <input
+        className='searchbar'
+        onChange={e => handleSetQuery(e.target.value)}
+        type="text"
+        name="searchbar"
+        id="searchbar"
+        placeholder="Search for a stock"
+      />
+      {results && (
+        <ul>
+          {results.reverse().map((item: IStock) => (
+            <li key={item._id}>
+              <Link href={`/${item.symbol}`}>
+                {item.symbol} - {item.name} - {item.exchange}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Searchbar;
