959ec8c5e1e81affc1b51467aacd34b49b25410e
Lee <leesheppard2404@gmail.com>
2023-08-16 19:11:01 +0200

    fixed type issues

--- src/app/components/InvestmentItem.tsx
+++ src/app/components/InvestmentItem.tsx
@@ -1,25 +1,56 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
+import { ObjectId } from 'mongodb';
 
-const InvestmentItem = ({ item }) => {
+type InvestmentItemProps = {
+  item: {
+    _id: ObjectId;
+    name: string;
+    exchange: string;
+    quantity: number;
+    buyPrice: string;
+    totalPrice: string;
+    currentWorth: string;
+  };
+};
+
+const InvestmentItem: React.FC<InvestmentItemProps> = ({ item }) => {
   return (
     <Link href={`/${item.name}`} className="card-item-layout">
       <article className="card-item">
         <div className="card-item__header">
           <p className="card-item__header-symbol">{item.name}</p>
           <p className="card-item__header-name">
-            {item.quantity} Shares @ {item.buyPrice.toFixed(2)}
+            {item.quantity} Shares @ {Number(item.buyPrice).toFixed(2)}
           </p>
         </div>
         <div className="card-item__info">
           {item.currentWorth < item.totalPrice ? (
-            <p className="card-item__price-red">{item.currentWorth.toFixed(2) === "NaN" ? <span>Loading...</span> : item.currentWorth.toFixed(2)} USD</p>
+            <p className="card-item__price-red">
+              {Number(item.currentWorth).toFixed(2) === 'NaN' ? (
+                <span>Loading...</span>
+              ) : (
+                Number(item.currentWorth).toFixed(2)
+              )}{' '}
+              USD
+            </p>
           ) : (
-            <p className="card-item__price-green">{item.currentWorth.toFixed(2) === "NaN" ? <span>Loading...</span> : item.currentWorth.toFixed(2)} USD</p>
+            <p className="card-item__price-green">
+              {Number(item.currentWorth).toFixed(2) === 'NaN' ? (
+                <span>Loading...</span>
+              ) : (
+                Number(item.currentWorth).toFixed(2)
+              )}{' '}
+              USD
+            </p>
           )}
           <p className="card-item__change">
-            {(((item.currentWorth - item.totalPrice) / Math.abs(item.totalPrice)) * 100).toFixed(2)}{' '}
+            {(
+              ((+item.currentWorth - +item.totalPrice) /
+                Math.abs(+item.totalPrice)) *
+              100
+            ).toFixed(2)}{' '}
             %
           </p>
         </div>
